171e50ab7a8129a97caa6dc45bc87d5d1c4c3001
Nurbolat <nurba.dav@gmail.com>
2023-08-28 14:20:55 +0600

    attempt 1

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,19 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> projectsOfGivenDev = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> projectInfo : projects.entrySet()) {
+            if (projectInfo.getValue().contains(developer))
+                projectsOfGivenDev.add(projectInfo.getKey());
+        }
+        projectsOfGivenDev.sort((s1, s2) -> {
+            if (s1.length() > s2.length()) return -1;
+            if (s1.length() < s2.length()) return 1;
+            return s2.compareTo(s1);
+        });
+        return projectsOfGivenDev;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,36 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> result = new HashMap<>();
+        sourceMap.keySet().forEach(s -> {
+            Set<String> entry = result.getOrDefault(s.length(), new HashSet<>());
+            entry.add(s);
+            result.put(s.length(), entry);
+        });
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MapFromKeysCreator main = new MapFromKeysCreator();
+        main.createMap(
+                Map.of("Monday", 1,
+                        "Tuesday", 2,
+                        "Wednesday", 3,
+                        "Thursday", 4,
+                        "Friday", 5)).forEach((integer, strings) -> {
+            System.out.println(integer);
+            strings.forEach(System.out::println);
+        });
+
+//                Map.of(6, Set.of("Monday", "Friday"),
+//                        7, Set.of("Tuesday"),
+//                        8, Set.of("Thursday"),
+//                        9, Set.of("Wednesday"));
     }
 }
